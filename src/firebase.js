4c86685e1c76fcbf227a66c89890e7438123f172
Somesh Debnath <91829500+Somesh-Debnath@users.noreply.github.com>
2022-07-06 17:26:43 +0530

    Setup initial app and created navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAwecXecvC7eE6sUUGdHff4Pcd7DKoxv-g",
+    authDomain: "clone-80f0c.firebaseapp.com",
+    projectId: "clone-80f0c",
+    storageBucket: "clone-80f0c.appspot.com",
+    messagingSenderId: "268884594055",
+    appId: "1:268884594055:web:4d5410488158f975099722",
+    measurementId: "G-C9YDXS7L6R"
+  };
